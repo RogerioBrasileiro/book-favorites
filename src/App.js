d39b3b55f0165a697f8a9f0d2db72c67c3aea391
Rafael <99349794+RafaelGadelhaSantiago@users.noreply.github.com>
2022-09-17 22:30:37 -0300

    Login Component
    Login component rebild.
    
    Co-Authored-By: RogerioBrasileiro <105891672+RogerioBrasileiro@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -7,24 +7,17 @@ import Login from './components/Login';
 function App() {
 
   return (
+
     <BrowserRouter>
-      <h1>Book Favorites</h1>
-      <nav>
-        <ul>
-          <li>
-            <link to='/'>Home</link>
-          </li>
-          <li>
-            <link to='/login'>Login</link>
-          </li>
-        </ul>
-      </nav>
+    
+      <Login />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<h1>Não tenho essa página</h1>} />
       </Routes>
     </BrowserRouter>
+
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Figure from 'react-bootstrap/Figure'
+// import '../images/book.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Login.css';
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <Form className="container col-4 center-screen">
+
+      <Figure>
+        <Figure.Image
+          width={171}
+          height={180}
+          alt="171x180"
+          src="../images/book.jpg"
+        />
+      </Figure>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control type="email" placeholder="Email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Form.Group className="center-me" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Permanecer conectado" />
+      </Form.Group>
+
+      <div className="d-grid gap-2 mb-3 mt-3">
+        <Button variant="primary" size="lg">
+          Log in
+        </Button>
+      </div>
+    </Form>
+  );
 }
-
-
